77a8107506145d3b95a97552ca7e252ca2be7e2a
Rudy Ravelin <rudy@noukod.com>
2019-04-05 18:34:58 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -9,19 +9,10 @@ class App extends Component {
     timerIDs: []
   }
 
+  componentDidMount() {
+    this.handleAddTimer();
+  }
 
-  //Your code here:
-
-
-
-
-
-
-
-
-
-  // No need to modify anything in render or the class methods below
-  // Unless, of course, you're curious about how it all works
   render() {
 
     return (
